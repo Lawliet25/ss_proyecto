ef043b8a85ef62c6c22a33efba128a65d505c9e2
Rodrigo Valle <fernandovalle939@gmail.com>
2022-07-04 19:34:07 -0600

    Se creó login funcional

--- resources/views/layout/template.blade.php
+++ resources/views/layout/template.blade.php
@@ -29,8 +29,8 @@
       }
     </style>
     <nav class="navbar navbar-light" style="background-color:#003566;">
-        <div class="container-fluid">
-          <div class="navbar-header">
+        <div class="container-fluid" >
+          <div class="navbar-header" >
             <button type="button" class="navbar-toggle collapsed"
                     data-toggle="collapse" data-target="#navbar"
                     aria-expanded="false" aria-controls="navbar">
@@ -39,10 +39,14 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
+
               <a class="navbar-brand" href="{{route('pre.index')}}">Bachillerato Virtual</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
+              @guest
+              <li><a style="font-size:17px;" href="{{route('login')}}">Login</a></li>
+              @else
                 <li class="dropdown">
                 <a href="{{route('pre.index')}}">Alumnos PreRegistrados</a>
                 </li>
@@ -61,7 +65,12 @@
             </ul>
             <ul class="nav navbar-nav navbar-right">
 
-                <li><a href="#">Usuario (cerrar sesión)</a></li>
+                <form style="margin-top:12px; font-size:15px;" class="" action="{{route('logout')}}" method="post">
+                @csrf
+                  <a style="" href="#" onclick="this.closest('form').submit()">Cerrar sesión</a>
+                </form>
+
+                @endguest
             </ul>
 
           </div>
