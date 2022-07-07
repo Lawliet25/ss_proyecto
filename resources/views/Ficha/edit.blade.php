f8ea4de6e5d70cf3373e2ebbad12880108bb0bfb
Alejandro Linares <oscar.alejandro.linares@gmail.com>
2022-07-07 12:07:09 -0600

    Agregando secciones

--- resources/views/Ficha/edit.blade.php
+++ resources/views/Ficha/edit.blade.php
@@ -735,6 +735,29 @@
                                  <option>A</option>
                                  <option>B</option>
                                  <option>C</option>
+                                 <option>D</option>
+                                 <option>E</option>
+                                 <option>F</option>
+                                 <option>G</option>
+                                 <option>H</option>
+                                 <option>I</option>
+                                 <option>J</option>
+                                 <option>K</option>
+                                 <option>L</option>
+                                 <option>M</option>
+                                 <option>N</option>
+                                 <option>O</option>
+                                 <option>P</option>
+                                 <option>Q</option>
+                                 <option>R</option>
+                                 <option>S</option>
+                                 <option>T</option>
+                                 <option>U</option>
+                                 <option>V</option>
+                                 <option>W</option>
+                                 <option>X</option>
+                                 <option>Y</option>
+                                 <option>Z</option>
                               </select>
                          </div>
                       </div>
